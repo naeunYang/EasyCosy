ccf36368a8f889882880c52d48c2a0fc186f94b7
naeunYang <didskdms2@naver.com>
2024-01-13 03:30:12 +0900

    first commit

---
+++ src/main/java/com/springboot/easycosy/EasyCosyApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.easycosy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyCosyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyCosyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/easycosy/EasyCosyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.easycosy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyCosyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
